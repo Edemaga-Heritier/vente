367dcfc821af9583fbe63c80ff7889245776c28f
Edemaga-Heritier <hedemaga@gmail.com>
2024-12-01 17:29:08 +0100

    ajoutter l'operation de suppression

--- app.js
+++ app.js
@@ -63,4 +63,10 @@ app.put('/api/stuff/:id', (req, res, next) => {
   .then(() => res.status(200).json({ message: 'Objet modifié !'}))
   .catch(error => res.status(400).json({ error }))
 })
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }))
+})
 module.exports =app
