463ade3155a0d7c09d1d803a213c52b0b2d45250
Edemaga-Heritier <hedemaga@gmail.com>
2024-12-02 15:30:30 +0100

    ajoute de package fs

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,50 @@
 const Thing = require('../Models/thing');
+const fs=require('fs');
+
+
+exports.deleteThing = (req, res, next) => {
+   Thing.findOne({ _id: req.params.id})
+       .then(thing => {
+           if (thing.userId != req.auth.userId) {
+               res.status(401).json({message: 'Not authorized'});
+           } else {
+               const filename = thing.imageUrl.split('/images/')[1];
+               fs.unlink(`images/${filename}`, () => {
+                   Thing.deleteOne({_id: req.params.id})
+                       .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                       .catch(error => res.status(401).json({ error }));
+               });
+           }
+       })
+       .catch( error => {
+           res.status(500).json({ error });
+       });
+};
+
+exports.modifyThing =(req,res,next) => {
+ const thingObject = req.file ? {
+       ...JSON.parse(req.body.thing),
+       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+   } : { ...req.body };
+ 
+   delete thingObject._userId;
+   Thing.findOne({_id: req.params.id})
+       .then((thing) => {
+           if (thing.userId != req.auth.userId) {
+               res.status(401).json({ message : 'Not authorized'});
+           } else {
+               Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
+               .then(() => res.status(200).json({message : 'Objet modifié!'}))
+               .catch(error => res.status(401).json({ error }));
+           }
+       })
+       .catch((error) => {
+           res.status(400).json({ error });
+       });
+}
+
+
+
 
 
 exports.createThing = (req, res, next) => {
@@ -79,21 +125,21 @@ exports.modifyThing = (req, res, next) => {
   );
 };
 
-exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-};
+// exports.deleteThing = (req, res, next) => {
+//   Thing.deleteOne({_id: req.params.id}).then(
+//     () => {
+//       res.status(200).json({
+//         message: 'Deleted!'
+//       });
+//     }
+//   ).catch(
+//     (error) => {
+//       res.status(400).json({
+//         error: error
+//       });
+//     }
+//   );
+// };
 
 exports.getAllStuff = (req, res, next) => {
   Thing.find().then(
