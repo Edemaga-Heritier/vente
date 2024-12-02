3074e6ef436c1b88c0e8f140acb6bc20388a516b
Edemaga-Heritier <hedemaga@gmail.com>
2024-12-02 11:44:46 +0100

    la logique de login dans le controller

--- controllers/user.js
+++ controllers/user.js
@@ -17,5 +17,28 @@ bcrypt.hash('req.body.password',10)
 
 }
 exports.login=(req, res, next) =>{
+   User.findOne({email: req.body.email})
+   .then((user)=>{
+if(user === nul){
+    res.status(404).json({message:'Paire identifiant/mot de pass incorrect'})
+} else{
+    bcrypt.compare(req.body.password, user.password)
+   .then(valid =>{
+    if(!valid){
+        res.status(404).json({message:'Paire identifiant/mot de pass incorrect'})
+    }else{
+        res.status(200).json({
+            userId: user._id,
+            token:'TOKEN'
+        })
+    }
+    
+   })
+   .catch(error=>res.status(500).json({error}))
+
+}
+
+   })
+.catch(error=>res.status(500).json({error}))
 
 }
